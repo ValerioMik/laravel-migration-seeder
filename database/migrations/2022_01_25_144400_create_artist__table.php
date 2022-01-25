9a745348e700ceed6c32c708333f1f3949c8149b
ValerioMik <valerio.avallone@hotmail.com>
2022-01-25 16:26:04 +0100

    inserite le tabbelle

---
+++ database/migrations/2022_01_25_144400_create_artist__table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArtistTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('artist_', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('surname');
+            $table->date('date_of_birth');
+            $table->string('gender',10);
+            $table->text('biography');
+            $table->bigInteger('records_sold');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('artist_');
+    }
+}
---
+++ database/migrations/2022_01_25_151933_create_songs__table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSongsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('songs_', function (Blueprint $table) {
+            $table->id();
+            $table->string('song_name');
+            $table->date('release_date');
+            $table->string('reference_album');
+            $table->text('song_name');
+            $table->bigInteger('records_sold');
+            $table->string('record company');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('songs_');
+    }
+}
